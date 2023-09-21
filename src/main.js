ab71cb92e49b72eec094b75edf37f8c604a1f357
Mudassir007 <mudassir.cuiatd@gmail.com>
2023-09-21 18:01:26 +0500

    til sep 21

--- src/main.js
+++ src/main.js
@@ -1,4 +1,86 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import AllComponentsView from './views/AllComponentsView.vue';
+import CartView from './views/CartView.vue';
+import AddressView from "@/views/AddressView";
+import SummaryView from "@/views/SummaryView";
+import   Keycloak from "keycloak-js";
 
-createApp(App).mount('#app')
+
+const routes = [
+  { path: '/', component: AllComponentsView },
+  { path: '/cart/', component: CartView },
+  { path: '/cart/address', component: AddressView },
+  { path: '/cart/address/summary', component: SummaryView }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
+const keycloakConfig = {
+  url: 'http://localhost:8080/',
+  realm: 'fishfarmpk',
+  clientId: 'test-1',
+    onLoad:'login_required',
+    public:true
+
+};
+
+const keycloak =  new Keycloak(keycloakConfig);
+
+keycloak
+.init({ onLoad: 'check-sso', promiseType: 'native'})
+.then((authenticated) => {
+  if (authenticated) {
+    console.log('User is authenticated');
+    localStorage.setItem('accessToken', keycloak.token);
+    localStorage.setItem('vueRefreshToken', keycloak.refreshToken);
+    localStorage.setItem('idToken', keycloak.idToken)
+
+    // startApp();
+  } else {
+    console.log('User is not authenticated');
+  }
+})
+.catch((error) => {
+  console.error('Keycloak initialization error:', error);
+});
+
+      keycloak.onAuthSuccess = () => {
+      setInterval(() => {
+        keycloak
+            .updateToken(60)
+            .then((refreshed) => {
+            if (refreshed) {
+                console.debug('Token refreshed', refreshed);
+                console.log(keycloak.token)
+                localStorage.setItem('accessToken', keycloak.token);
+                localStorage.setItem('vueRefreshToken', keycloak.refreshToken);
+
+              } else {
+                console.warn(
+                'Token not refreshed, valid for ' +
+                    Math.round(
+                    (keycloak.tokenParsed.exp +
+                        keycloak.timeSkew -
+                        new Date().getTime()) /
+                        1000
+                    ) +
+                    ' seconds'
+                );
+            }
+            })
+            .catch((error) => {
+            console.log(error);
+            });
+        }, 60000);
+      };
+
+      export default keycloak
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://127.0.0.1:8000', // Change this to your FastAPI server URL
+        changeOrigin: true,
+        logLevel: 'debug',
+      },
+    },
+  },
+};
